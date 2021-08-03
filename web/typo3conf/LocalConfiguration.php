161edd8d8250d43fa449a81d0146c659482cbd86
Dan Untenzu <mail@pixelbrackets.de>
2021-08-03 17:29:37 +0200

    [FEATURE] Upgrade to TYPO3 10
    Upgrade skeleton from TYPO3 version 7 to 10.

--- web/typo3conf/LocalConfiguration.php
+++ web/typo3conf/LocalConfiguration.php
@@ -1,88 +1,131 @@
 <?php
-
-// ** Read MySQL service properties from _ENV['VCAP_SERVICES']
-$services = json_decode($_ENV['VCAP_SERVICES'], true);
-$service = $services['cleardb'][0];  // pick the first MySQL service
-
-return array(
-    'BE' => array(
+return [
+    'BE' => [
         'debug' => false,
         'explicitADmode' => 'explicitAllow',
-        'installToolPassword' => '$P$CbLBI.8b2iFtQF08sX.EAGudttep/E1',
-        'loginSecurityLevel' => 'rsa',
-    ),
-    'DB' => array(
-        'database' => $service['credentials']['name'],
-        'host' => $service['credentials']['hostname'],
-        'password' => $service['credentials']['password'],
-        'port' => 3306,
-        'username' => $service['credentials']['username'],
-    ),
-    'EXT' => array(
-        'extConf' => array(
-            'rsaauth' => 'a:1:{s:18:"temporaryDirectory";s:0:"";}',
-            'saltedpasswords' => 'a:2:{s:3:"BE.";a:4:{s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}s:3:"FE.";a:5:{s:7:"enabled";i:1;s:21:"saltedPWHashingMethod";s:41:"TYPO3\\CMS\\Saltedpasswords\\Salt\\PhpassSalt";s:11:"forceSalted";i:0;s:15:"onlyAuthService";i:0;s:12:"updatePasswd";i:1;}}',
-        ),
-    ),
-    'FE' => array(
+        'installToolPassword' => '$argon2i$v=19$m=65536,t=16,p=1$bXpKc2hOM0NsU0xRak4uSQ$Eva79XFkvMjBQBj2cWs+Jx/+RlujX8az3wmrdFGEZgw',
+        'loginSecurityLevel' => 'normal',
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'DB' => [
+        'Connections' => [
+            'Default' => [
+                'charset' => 'utf8mb4',
+                'dbname' => 'xxxxxxxxxx',
+                'driver' => 'mysqli',
+                'host' => 'localhost',
+                'password' => 'xxxxxxxxxx',
+                'tableoptions' => [
+                    'charset' => 'utf8mb4',
+                    'collate' => 'utf8mb4_unicode_ci',
+                ],
+                'user' => 'xxxxxxxxxx',
+            ],
+        ],
+    ],
+    'EXTENSIONS' => [
+        'backend' => [
+            'backendFavicon' => '',
+            'backendLogo' => '',
+            'loginBackgroundImage' => '',
+            'loginFootnote' => '',
+            'loginHighlightColor' => '',
+            'loginLogo' => '',
+        ],
+        'extensionmanager' => [
+            'automaticInstallation' => '1',
+            'offlineMode' => '0',
+        ],
+    ],
+    'FE' => [
         'debug' => false,
-        'loginSecurityLevel' => 'rsa',
-    ),
-    'GFX' => array(
-        'colorspace' => 'sRGB',
-        'im' => 1,
-        'im_mask_temp_ext_gif' => 1,
-        'im_path' => '/usr/bin/',
-        'im_path_lzw' => '/usr/bin/',
-        'im_v5effects' => 1,
-        'im_version_5' => 'im6',
-        'image_processing' => 1,
-        'jpg_quality' => '80',
-    ),
-    'INSTALL' => array(
-        'wizardDone' => array(
-            'TYPO3\CMS\Install\Updates\BackendUserStartModuleUpdate' => 1,
-            'TYPO3\CMS\Install\Updates\FileListIsStartModuleUpdate' => 1,
-            'TYPO3\CMS\Install\Updates\FilesReplacePermissionUpdate' => 1,
-            'TYPO3\CMS\Install\Updates\LanguageIsoCodeUpdate' => 1,
-            'TYPO3\CMS\Install\Updates\MigrateShortcutUrlsAgainUpdate' => 1,
-            'TYPO3\CMS\Install\Updates\PageShortcutParentUpdate' => 1,
-            'TYPO3\CMS\Install\Updates\ProcessedFileChecksumUpdate' => 1,
-            'TYPO3\CMS\Install\Updates\TableFlexFormToTtContentFieldsUpdate' => 1,
-            'TYPO3\CMS\Rtehtmlarea\Hook\Install\DeprecatedRteProperties' => 1,
-            'TYPO3\CMS\Rtehtmlarea\Hook\Install\RteAcronymButtonRenamedToAbbreviation' => 1,
-        ),
-    ),
-    'MAIL' => array(
-        'transport_sendmail_command' => ' -t -i ',
-    ),
-    'SYS' => array(
-        'caching' => array(
-            'cacheConfigurations' => array(
-                'extbase_object' => array(
+        'disableNoCacheParameter' => true,
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'GFX' => [
+        'processor' => 'GraphicsMagick',
+        'processor_allowTemporaryMasksAsPng' => false,
+        'processor_colorspace' => 'RGB',
+        'processor_effects' => false,
+        'processor_enabled' => true,
+        'processor_path' => '/usr/bin/',
+        'processor_path_lzw' => '/usr/bin/',
+    ],
+    'LOG' => [
+        'TYPO3' => [
+            'CMS' => [
+                'deprecations' => [
+                    'writerConfiguration' => [
+                        'notice' => [
+                            'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
+                                'disabled' => true,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'MAIL' => [
+        'transport' => 'sendmail',
+        'transport_sendmail_command' => '/usr/sbin/sendmail -t -i',
+        'transport_smtp_encrypt' => '',
+        'transport_smtp_password' => '',
+        'transport_smtp_server' => '',
+        'transport_smtp_username' => '',
+    ],
+    'SYS' => [
+        'caching' => [
+            'cacheConfigurations' => [
+                'hash' => [
                     'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
-                    'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend',
-                    'groups' => array(
-                        'system',
-                    ),
-                    'options' => array(
-                        'defaultLifetime' => 0,
-                    ),
-                ),
-            ),
-        ),
-        'clearCacheSystem' => false,
+                ],
+                'imagesizes' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'pages' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'pagesection' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'rootline' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+            ],
+        ],
         'devIPmask' => '',
         'displayErrors' => 0,
-        'enableDeprecationLog' => false,
-        'encryptionKey' => getenv('TYPO3_SYS_ENCRYPTIONKEY'),
-        'isInitialDatabaseImportDone' => false,
-        'isInitialInstallationInProgress' => true,
-        'sitename' => getenv('TYPO3_SYS_SITENAME'),
-        'sqlDebug' => 0,
-        'systemLogLevel' => 2,
-        't3lib_cs_convMethod' => 'iconv',
-        't3lib_cs_utils' => 'iconv',
-    ),
-);
-?>
+        'encryptionKey' => '8e5b2f48299c4927348690d57d7288d62723ee33ecdca64973bc877d079dc50129eccb406a20a1ca38828259fc4c0300',
+        'exceptionalErrors' => 4096,
+        'features' => [
+            'felogin.extbase' => true,
+            'fluidBasedPageModule' => true,
+            'rearrangedRedirectMiddlewares' => true,
+            'unifiedPageTranslationHandling' => true,
+            'yamlImportsFollowDeclarationOrder' => true,
+        ],
+        'sitename' => 'ACME',
+        'systemMaintainers' => [
+            1,
+        ],
+    ],
+];
